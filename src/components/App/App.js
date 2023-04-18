a73fab68d9e6e48b62f899208e94269210e04ad9
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-04-18 13:19:18 -0700

    Create nav bar basic styling/ layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,15 @@
 import React, { useState, useEffect } from "react";
+import { HomePage } from "../HomePage/HomePage";
+import { Navigation } from "../Navigation/Navigation";
 import "./App.css";
 
 export const App = () => {
+  
 
+  return (
+    <main>
+      <Navigation />
+      <HomePage />
+    </main>
+  )
 }
