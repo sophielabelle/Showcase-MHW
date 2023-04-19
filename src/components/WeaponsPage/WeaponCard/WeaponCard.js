e22e9ae0ce573d9bffb46bb9ce32fc5dad5621a3
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-04-19 14:28:09 -0700

    Add weapon iconds and change styling to show images as cards

--- src/components/WeaponsPage/WeaponCard/WeaponCard.js
+++ src/components/WeaponsPage/WeaponCard/WeaponCard.js
@@ -3,13 +3,20 @@ import { Link } from "react-router-dom";
 import "./WeaponCard.css";
 
 export const WeaponCard = ({weapon, setSelected}) => {
+  let icon;
+  if(weapon.assets === null) {
+    icon = null
+  } else {
+    icon = weapon.assets.icon
+  }
   const id = weapon.id
-  
+  console.log(weapon)
   return (
     <div className="weapon-card">
+      <img className="icon" src={icon} alt={weapon.name}/>
       <p className="weapon-name">{weapon.name}</p>
       <Link to={`all-weapons/weapon/${id}`}>
-        <button onClick={()=> {
+        <button className="select-btn" onClick={()=> {
         setSelected(weapon)
         }}>View Weapon</button>
       </Link>
--- src/components/WeaponsPage/WeaponsPage.js
+++ src/components/WeaponsPage/WeaponsPage.js
@@ -4,11 +4,15 @@ import "./WeaponsPage.css";
 
 export const WeaponsPage = ({allWeapons, setSelected}) => {
   const weaponCards = allWeapons.map(weapon => <WeaponCard key={weapon.name} weapon={weapon} setSelected={setSelected}/>)
-
+  const display = weaponCards.length ? 
+    <div className="weapon-cards">
+      {weaponCards}
+    </div> 
+    : <h3>Loading...</h3>
   return (
     <section className="weapons-page">
       <h2>All MHW Weapons</h2>
-      {weaponCards}
+      {display}
     </section>
   )
 }
