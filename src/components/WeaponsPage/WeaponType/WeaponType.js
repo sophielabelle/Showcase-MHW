46c3bcce619c3980eb26f7883dda781a7287f8a1
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-04-19 16:48:13 -0700

    Add formating to the weapon type sections

--- src/components/WeaponsPage/WeaponType/WeaponType.js
+++ src/components/WeaponsPage/WeaponType/WeaponType.js
@@ -3,13 +3,15 @@ import { WeaponCard } from "./WeaponCard/WeaponCard";
 import "./WeaponType.css";
 
 export const WeaponType = ({name, typeOfWeapons, setSelected}) => {
-  console.log('type', typeOfWeapons)
   const weaponCards = typeOfWeapons.map(weapon => <WeaponCard key={weapon.name} weapon={weapon} setSelected={setSelected}/>)
   const display = weaponCards.length ? <div className="weapon-cards">{weaponCards}</div> : <h3>Loading...</h3>
 
+  const displayType = name.replaceAll('-', ' ').toUpperCase()
+  console.log(displayType)
+
   return (
-    <section className="weapons-page">
-      <h3>{name}</h3>
+    <section className="weapon-type-categories">
+      <h3>{displayType}</h3>
       {display}
     </section>
   )
