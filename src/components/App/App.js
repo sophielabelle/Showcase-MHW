d92471043c3d184c4a9294676190a29c5288aa4e
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-04-19 09:44:30 -0700

    Add Router and routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,29 +1,29 @@
 import React, { useState, useEffect } from "react";
 import { HomePage } from "../HomePage/HomePage";
 import { Navigation } from "../Navigation/Navigation";
-import "./App.css";
-import { fetchData } from "../../data/apiCalls";
 import { ArmorPage } from "../ArmorPage/ArmorPage";
+import { fetchData } from "../../data/apiCalls";
+import { Redirect, Route, Switch } from "react-router-dom";
+import "./App.css";
 
 export const App = () => {
   const [randomSet, setRandomSet] = useState('');
   const [allArmor, setAllArmor] = useState([]);
 
-  // const fetchRandomArmor = () => {
-  //   fetchData('armor/sets/10')
-  //     .then(data => {
-  //       // console.log(data.pieces[0].assets.imageFemale)
-  //       setRandomSet(data.pieces[0].assets.imageFemale)
-  //     })
-  //     .catch(err => {
-  //       console.log('error',err.message)
-  //     })
-  // }
+  const fetchRandomArmor = () => {
+    fetchData('armor/sets/10')
+      .then(data => {
+        // console.log(data.pieces[0].assets.imageFemale)
+        setRandomSet(data.pieces[0].assets.imageFemale)
+      })
+      .catch(err => {
+        console.log('error',err.message)
+      })
+  }
 
   const fetchAllArmor = () => {
     fetchData('armor/sets')
       .then(data => {
-        console.log(data)
         setAllArmor(data)
       })
       .catch(err => {
@@ -31,9 +31,9 @@ export const App = () => {
       })
   }
 
-  // useEffect(() => {
-  //   fetchRandomArmor()
-  // }, [])
+  useEffect(() => {
+    fetchRandomArmor()
+  }, [])
 
   useEffect(() => {
     fetchAllArmor()
@@ -42,8 +42,12 @@ export const App = () => {
   return (
     <main className="App">
       <Navigation />
-      {/* <HomePage randomArmor={randomSet} /> */}
-      <ArmorPage allArmors={allArmor} />
+      <Switch>
+        <Route path="/all-armors" render={() => <ArmorPage allArmors={allArmor} /> } />
+        {/* <Route path="/all-armors" render={() => <} /> */}
+        <Route path="/" render={() => <HomePage randomArmor={randomSet} /> } />
+        <Redirect from="*" to="/" />
+      </Switch>
     </main>
   )
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,16 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { SearchForm } from "./SearchForm/SearchForm";
+import { NavLink } from "react-router-dom";
 import "./Navigation.css"
 
 export const Navigation = () => {
   return (
     <nav>
       <SearchForm />
-      <h1>Monster Hunter World Armor Wiki</h1>
+      <NavLink to="/" style={{ textDecoration: 'none', color: 'white' }}>
+        <h1>Monster Hunter World Armor Wiki</h1>
+      </NavLink>
       <div className="nav-btns">
-        <button className="nav-btn">Home</button>
-        <button className="nav-btn">All Armor Sets</button>
-        <button className="nav-btn">Your Armors</button>
+        <NavLink to="/">
+          <button className="nav-btn">Home</button>
+        </NavLink>
+        <NavLink to="/all-armors">
+          <button className="nav-btn">All Armor Sets</button>
+        </NavLink>
+        <NavLink to="/">
+          <button className="nav-btn">Your Armors</button>
+        </NavLink>
       </div>
     </nav>
   )
