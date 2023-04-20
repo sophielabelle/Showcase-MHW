4730d7d9b190e73fc5323e4b37f83a79fde27ff0
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-04-20 09:58:09 -0700

    Add cypress and first test

---
+++ cypress/e2e/Home.cy.js
@@ -0,0 +1,17 @@
+describe('Home Page', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'https://mhw-db.com/weapons', {
+      fixture: "randomWeapon.json"
+    })
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('Should display a welcome introduction message', () => {
+    cy.get('.home-instructions').find('h2').should('contain', 'Thanks for Stopping by!')
+    cy.get('.home-instructions').find('p').contains('Welcome I created this site in order to provide you a space to learn and keep track of all the weapons that are availabel within Monster Hunter World. You can reasearch armors you are interested in, search through all of them and maybe eventually save them. Have a look around and keep on playing.')
+  })
+
+  it('Should display a random weapon image on page load', () => {
+    cy.get('.random-weapon').find('img').should('have.attr', 'src').should('contain', 'https://assets.mhw-db.com/weapons/great-sword/83f7ab6e7e5669ec416d772049b8b054e2624c2e.c7bad811e203c9bb55626e414659a4f7.png')
+  })
+})
