9158bca5ca9030720f4319967f0bc86d5a463e59
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-04-21 15:43:36 -0700

    Update weapon page styling and conditional rendering based off the search state

--- src/components/WeaponsPage/WeaponsPage.js
+++ src/components/WeaponsPage/WeaponsPage.js
@@ -3,7 +3,7 @@ import { WeaponType } from "./WeaponType/WeaponType";
 import { WeaponCard } from "./WeaponType/WeaponCard/WeaponCard";
 import "./WeaponsPage.css";
 
-export const WeaponsPage = ({allWeapons, searchResults, setSelected, searchText}) => {
+export const WeaponsPage = ({allWeapons, searchText, searchResults, setSelected}) => {
   console.log('searched results', searchResults)
   const weaponSections = allWeapons.reduce((acc, weapon) => {
     if(acc[weapon.type]){
@@ -17,15 +17,16 @@ export const WeaponsPage = ({allWeapons, searchResults, setSelected, searchText}
   const types = keys.map(key => <WeaponType key={key} sectionName={key} typeOfWeapons={weaponSections[key]} setSelected={setSelected} />)
   const display = types.length ? <div className="weapon-types">{types}</div> : <h3>Loading...</h3>
   
-  if(!searchResults.length){
+  if(!searchText.length){
     return (
       <section className="weapons-page">
         <h2>All MHW Weapons</h2>
         {display}
       </section>
     )
-  } else {
-    const searchedTypes = searchResults.length ? searchResults.map(weapon => <WeaponCard key={weapon.id} weapon={weapon} setSelected={setSelected}/>) : <h3> We're sorry there are no weapons matching your search! </h3>
+  } else if(searchText.length) {
+    const searchedTypes = searchResults.length  ? searchResults.map(weapon => <WeaponCard key={weapon.id} weapon={weapon} setSelected={setSelected}/>) : <h3> We're sorry there are no weapons matching your search! </h3>
+    
     return (
       <section className="weapons-page">
         <h2>Search Results</h2>
