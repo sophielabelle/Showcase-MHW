e7a87e59e02c7ea37f1f2709ffeede16e3b14b68
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-04-22 15:18:22 -0700

    Add buttons to home page to view all weapons and a single weapon

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { RandomWeapon } from "./RandomWeapon/RandomWeapon";
+import { NavLink } from "react-router-dom";
 import "./HomePage.css";
 
 export const HomePage = ({weapon, setSelected}) => {
   return (
     <section className="home-page">
-      <div className="home-instructions">
-        <h2>Thanks for Stopping by!</h2>
-        <p>Welcome I created this site in order to provide you a space to learn and keep track of all the weapons that are availabel within Monster Hunter World. You can reasearch armors you are interested in, search through all of them and maybe eventually save them. Have a look around and keep on playing.</p>
+      <div className="home-page-boxes">
+        <div className="home-instructions">
+          <h2>Thanks for Stopping by!</h2>
+          <p>Welcome I created this site in order to provide you a space to learn and keep track of all the weapons that are availabel within Monster Hunter World. You can reasearch armors you are interested in, search through all of them and maybe eventually save them. Have a look around and keep on playing.</p>
+        </div>
+        <RandomWeapon weapon={weapon} setSelected={setSelected}/>
       </div>
-      <RandomWeapon weapon={weapon} setSelected={setSelected}/>
+      <NavLink to="/all-weapons">
+        <button className="nav-btn">View All Weapons</button>
+      </NavLink>
     </section>
   )
 }
--- src/components/HomePage/RandomWeapon/RandomWeapon.js
+++ src/components/HomePage/RandomWeapon/RandomWeapon.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "./RandomWeapon.css";
 
 export const RandomWeapon = ({weapon, setSelected}) => {
   const display = weapon.id ? 
