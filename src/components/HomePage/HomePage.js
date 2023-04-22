61333163c63dda4777b95492489ad81c6229d885
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-04-22 15:05:20 -0700

    Add a view weapon button to the random weapon preview on home page

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,14 +2,14 @@ import React from "react";
 import { RandomWeapon } from "./RandomWeapon/RandomWeapon";
 import "./HomePage.css";
 
-export const HomePage = ({weapon}) => {
+export const HomePage = ({weapon, setSelected}) => {
   return (
     <section className="home-page">
       <div className="home-instructions">
         <h2>Thanks for Stopping by!</h2>
         <p>Welcome I created this site in order to provide you a space to learn and keep track of all the weapons that are availabel within Monster Hunter World. You can reasearch armors you are interested in, search through all of them and maybe eventually save them. Have a look around and keep on playing.</p>
       </div>
-      <RandomWeapon weapon={weapon}/>
+      <RandomWeapon weapon={weapon} setSelected={setSelected}/>
     </section>
   )
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,26 +5,23 @@ import "./Navigation.css";
 
 export const Navigation = ({allWeapons, setSearchResults, searchText, setSearchText}) => {
   const location = useLocation()
-  const showHome = location.pathname !== "/home" ? <button className="nav-btn">Home</button> : <button className="nav-btn hidden">Home</button>
+  const showHome = location.pathname !== "/" ? <button className="nav-btn">Home</button> : <button className="nav-btn hidden">Home</button>
   const showWeapons = location.pathname !== "/all-weapons" ? <button className="nav-btn">All Weapons</button> : <button className="nav-btn hidden">All Weapons</button>
   const showSearchBar = location.pathname === "/all-weapons" ? <SearchForm allWeapons={allWeapons} setSearchResults={setSearchResults} searchText={searchText} setSearchText={setSearchText}/> : null
 
   return (
     <nav>
       {showSearchBar}
-      <NavLink to="/home" style={{ textDecoration: 'none', color: 'white' }}>
+      <NavLink to="/" style={{ textDecoration: 'none', color: 'white' }}>
         <h1>Monster Hunter World Weapon Wiki</h1>
       </NavLink>
       <div className="nav-btns">
-        <NavLink to="/home">
+        <NavLink to="/">
           {showHome}
         </NavLink>
         <NavLink to="/all-weapons">
           {showWeapons}
         </NavLink>
-        {/* <NavLink to="/favorite-weapons">
-          <button className="nav-btn">Favorite Weapons</button>
-        </NavLink> */}
       </div>
     </nav>
   )
