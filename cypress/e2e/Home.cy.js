6bd438b74537f9feb785bd9770d33eaaac5abebe
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-04-22 20:17:00 -0700

    Update cypress testing for the home page to include the added buttons on home page, add search testing and details on the home page:

--- cypress/e2e/Home.cy.js
+++ cypress/e2e/Home.cy.js
@@ -3,7 +3,7 @@ describe('Home Page', () => {
     cy.intercept('GET', 'https://mhw-db.com/weapons', {
       fixture: "randomWeapon.json"
     })
-    cy.visit('http://localhost:3000/')
+    cy.visit('https://monsterhunterworldweaponwiki.vercel.app/')
   })
 
   it('Should display a welcome introduction message', () => {
@@ -11,13 +11,25 @@ describe('Home Page', () => {
     cy.get('.home-instructions').find('p').contains('Welcome I created this site in order to provide you a space to learn and keep track of all the weapons that are availabel within Monster Hunter World. You can reasearch armors you are interested in, search through all of them and maybe eventually save them. Have a look around and keep on playing.')
   })
 
-  it('Should display a random weapon image on page load', () => {
+  it('Should display a random weapon image on page load with a button to access its details and its name', () => {
     cy.get('.random-weapon').find('img').should('have.attr', 'src').should('contain', 'https://assets.mhw-db.com/weapons/great-sword/83f7ab6e7e5669ec416d772049b8b054e2624c2e.c7bad811e203c9bb55626e414659a4f7.png')
+    cy.get('.random-weapon > p').contains('Buster Sword 1')
+    cy.get('.select-btn').contains('View Weapon')
   })
 
-  it('Should have a navigation bar at the top of the screen with a search bar, buttons and title', () => {
-    cy.get('h1').contains('Monster Hunter World Weapon Wiki')
-    cy.get('[href="/all-weapons"] > .nav-btn').contains('All Weapons')
-    cy.get('[href="/favorite-weapons"] > .nav-btn').contains('Favorite Weapons')
+  it('Should display the random weapons details after the button is clicked', () => {
+    cy.get('.select-btn').contains('View Weapon').click()
+    cy.get('h2').contains('Buster Sword 1')
+    cy.get('.selected-img').should('have.attr', 'src').should('contain', 'https://assets.mhw-db.com/weapons/great-sword/83f7ab6e7e5669ec416d772049b8b054e2624c2e.c7bad811e203c9bb55626e414659a4f7.png')
+    cy.get('.weapon-description > :nth-child(1)').contains('Weapon Type : great sword | Primary Damage Type : sever | Rarity : 1')
+    cy.get('.sharpness-container > p').contains('Base Weapon Sharpness Level')
+    cy.get('.sharpness-container').find('table')
+    cy.get('.weapon-description > :nth-child(3)').contains('The Buster Sword 1 has an attack base stat of 384 and the main damage type is sever. It has possible additional damage type of n/a and damage strength')
+    cy.get('.weapon-description > .active > .nav-btn').click().visit('https://monsterhunterworldweaponwiki.vercel.app/')
+  })
+
+  it('Should have a navigation bar at the top of the screen with a button and title', () => {
+    cy.get('nav').find('h1').contains('Monster Hunter World Weapon Wiki')
+    cy.get('.nav-btns > [href="/all-weapons"] > .nav-btn').contains('All Weapons')
   })
 })
--- cypress/e2e/WeaponsPage.cy.js
+++ cypress/e2e/WeaponsPage.cy.js
@@ -3,9 +3,9 @@ describe('Weapons Page', () => {
     cy.intercept('GET', 'https://mhw-db.com/weapons', {
       fixture: "allWeapons.json"
     })
-    cy.visit('http://localhost:3000/')
-    cy.get('[href="/all-weapons"] > .nav-btn').click()
-    cy.visit('http://localhost:3000/all-weapons')
+    cy.visit('https://monsterhunterworldweaponwiki.vercel.app/')
+    cy.get('.nav-btns > [href="/all-weapons"] > .nav-btn').click()
+    cy.visit('https://monsterhunterworldweaponwiki.vercel.app/all-weapons')
   })
 
   it('Should have a page title', () => {
@@ -29,6 +29,12 @@ describe('Weapons Page', () => {
     cy.get(':nth-child(2) > .weapon-cards > .weapon-card').contains('Buster Gun Lance 1')
     cy.get(':nth-child(2) > .weapon-cards > .weapon-card').find('.select-btn').contains('View Weapon')
     cy.get(':nth-child(2) > .weapon-cards > .weapon-card > .icon').should('have.attr', 'src').should('contain', 'https://assets.mhw-db.com/weapons/great-sword/icons/83b9e1fa727ca6ba922b53a42626a167.26ad6221e21811da5278502fabfc138b33d622bc.png')
+  })
+
+  it('Should be able to navigate to the weapons page from home bottom button', () => {
+    cy.get('[href="/"] > .nav-btn').click()
+    cy.get('.home-page > [href="/all-weapons"] > .nav-btn').click()
+    cy.get('.weapons-page')
   })
 
 })
